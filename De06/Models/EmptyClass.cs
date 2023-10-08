71e7836b88905302c99807371e8e5176af77b3f3
Malabeadman <deangelo.lee19@gmail.com>
2023-10-07 23:44:56 -0400

    .net start

---
+++ De06/Models/EmptyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace De06.Models
+{
+	public class EmptyClass
+	{
+		public EmptyClass()
+		{
+		}
+	}
+}
+
---
+++ De06/Scripts/DetroitQ.js
@@ -0,0 +1,11 @@
+﻿
+const url = 'https://services2.arcgis.com/qvkbeam7Wirps6zC/arcgis/rest/services/QLine_Stops/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json';
+let todos = [];
+
+function getItems() {
+    console.log("in js");
+  fetch(url)
+    .then(response => response.json())
+    .then(data => _displayItems(data))
+    .catch(error => console.error('Unable to get items.', error));
+}
